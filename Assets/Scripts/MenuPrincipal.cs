a0f4d58b1f675134880a0444350745ece1d6065a
Ale016R <ale016g@gmail.com>
2025-04-05 22:07:35 -0600

    menu principal

---
+++ Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public void IniciarJuego()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void CerrarAplicacion()
+    {
+
+        Application.Quit();
+
+    }
+}
